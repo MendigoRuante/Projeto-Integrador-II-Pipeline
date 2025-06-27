2b55b609ab6279495ee81c27e346f9356625e016
MendigoRuante <grindofred@gmail.com>
2025-06-27 15:34:55 -0300

    Add files via upload

---
+++ Sprint3/Arquivo_ASM.asm
@@ -0,0 +1,40 @@
+add $1, $0, $0
+add $2, $0, $0
+add $3, $0, $0
+add $4, $0, $0
+add $5, $0, $0
+add $6, $0, $0
+add $7, $0, $0
+NOP
+NOP
+NOP
+NOP
+NOP
+addi $1, $0, 3
+addi $2, $0, 5
+addi $3, $0, 1
+NOP
+NOP
+NOP
+NOP
+NOP
+beq $2, $0, 14
+NOP
+NOP
+NOP
+NOP
+NOP
+add $7, $7, $1
+sub $2, $2, $3
+NOP
+NOP
+NOP
+NOP
+NOP
+j 17
+NOP
+NOP
+NOP
+NOP
+NOP
+sw $7, 0($0)
